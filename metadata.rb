a49a8f5832d82b857d7a0b39569f6f960bf70070
Ben Dixon <ben@hillsbede.co.uk>
2013-06-20 21:13:30 +0100

    adds metadata

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name              "redis-tlq"
+maintainer        "Ben Dixon"
+maintainer_email  "ben@hillsbede.co.uk"
+description       "Installs redis from rwky's ppa"
+version           "0.0.1"
+
+recipe "redis-tlq", "Installs redis"
+
+supports "ubuntu"
