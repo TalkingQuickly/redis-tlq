eaaafe37878f219fb65eef71507eb356ca057ddb
Ben Dixon <ben@hillsbede.co.uk>
2013-10-12 20:27:26 +0100

    Update email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name              "redis-tlq"
 maintainer        "Ben Dixon"
-maintainer_email  "ben@hillsbede.co.uk"
+maintainer_email  "ben@talkingquickly.co.uk"
 description       "Installs redis from rwky's ppa"
 version           "0.0.1"
 
